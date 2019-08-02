b6955dd1fc6b5bc41bb7f2ab7234eba02ca660d0
Daniel Lee <leedc@me.com>
2019-08-02 03:14:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,23 @@
 #convert string to array
 
+# def reverse_each_word(sentence)
+#   # split each word into array
+#   words = sentence.split(" ")
+#   # set empty array for reverse sentence
+#   array = []
+#   # iterate through each word in words array
+#   # and return each index in reverse
+#   array = words.collect { |word| word.reverse }
+#   # join array indexes with space in between
+#   array.join(" ")
+# end
+
 def reverse_each_word(sentence)
-  # split each word into array
   words = sentence.split(" ")
-  # set empty array for reverse sentence
-  array = []
-  # iterate through each word in words array
-  # and return each index in reverse
-  array = words.collect { |word| word.reverse }
-  # join array indexes with space in between
-  array.join(" ")
+
+  def reverse(words)
+    words.collect { |word| word.reverse }
+  end
+
+  reverse(words).join(" ")
 end
